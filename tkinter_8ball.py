f90ee2cf829dbfe33adf4a2030913686c0eb5edb
VladoVladVlaVlV <vvojdanovski@fwparker.org>
2017-04-24 20:42:27 -0500

    commit2

--- tkinter_8ball.py
+++ tkinter_8ball.py
@@ -12,3 +12,59 @@
 # - Add a Label widget to display the answer (set to a initial value of "Your Fortune Here" or "--" or similar)
 # - Get your random answer message from a list of at least 10 possible strings. (e.g. ["Yes", "No", "Most Likely", "Definitely", etc...])
 # - Add THREE or more other style modifications to make your app unique (font family, font size, color, padding, image, borders, justification, whatever you can find in tkinter library etc.)  Make a comment at the top or bottom of your code to tell me your 3 things you did. (Just to help me out in checking your assignment)
+from tkinter import *
+from tkinter import font
+import random
+
+
+# noinspection PyBroadException
+class App():
+
+
+
+    def __init__(self,master):
+        self.myString = StringVar()
+        self.answer=StringVar()
+        self.myString.set("------")
+
+        self.title_font=font.Font(family="Comic Sans MS", size=17)
+        self.new_font=font.Font(family="Comic Sans MS", size=23)
+
+        self.question_button = Label(master, text="Type your question : ", font=self.title_font,bg="black",fg="gold")
+        self.question_button.grid(row=3, column=0, sticky="e" + "w")
+
+        self.enter = Entry(master, textvariable=self.myString,font=self.title_font,borderwidth=5,highlightcolor="black",justify="center")
+        self.enter.grid(row=3, column=1,sticky="e"+"w")
+
+        self.button = Label(master, text="ASK!", font=self.title_font,bg="black",fg="gold")
+        self.button.bind("<Button-1>", self.concat)
+        self.button.grid(row=3, column=2,sticky="e"+"w")
+
+        self.answer_label = Label(master, textvariable=self.answer, font=self.new_font,fg="red", width=10, borderwidth=5)
+        self.answer_label.grid(row=4, column=0,columnspan=3, sticky="w" + "e")
+
+    def concat(self, event):
+        answer_list=["Does not matter","Irrelevant","I do not care","Ehhh.... Whatever, Sure","No one cares","Leave me alone","Meh","Whatever","No.","Definitely No"]
+        self.answer.set("Answer: "+answer_list[random.randrange(0,9)])
+
+
+    #def concat(self,event):
+       # try:
+      #      self.grav.set(self.m1.get() * self.m2.get() / (self.r.get() * self.r.get()) * 0.0000000000667)
+      #  except ZeroDivisionError:
+      #      self.grav.set("Not divisible by 0")
+       # except:
+       #     self.grav.set("Type Error")
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Question Genie(Magic 8-Ball")
+    my_app = App(root)
+
+    img = PhotoImage(file="images/8_ball.gif")
+    img.bg="white"
+
+    Label(root, image=img).grid(row=0,column=1,sticky="w")
+
+
+    root.mainloop()
+
--- tkinter_universal_gravity_calculator.py
+++ tkinter_universal_gravity_calculator.py
@@ -18,4 +18,60 @@
 # - When "Calculate" is pushed, the result is displayed.  Yay!
 # - Add an exception for the possible entry of zero radius (ZeroDivisionError Exception)
 # - Make your app unique by changing 3 or more DIFFERENT style attributes or kwargs for your app.  Perhaps consider: fonts, color, padding, widths, etc).  Put a comment in your code to tell me what you changed. If you change the font for all the widgets, that is ONE thing.  If you add padx to all your app widgets, that is ONE thing.  If you change the color of all your blocks, that is ONE thing.
+from tkinter import *
+from tkinter import font
+
+
+# noinspection PyBroadException
+class App():
+
+
+
+    def __init__(self,master):
+        self.m1 = DoubleVar()
+        self.m2 = DoubleVar()
+        self.grav = DoubleVar()
+        self.r= DoubleVar()
+        self.title_font=font.Font(family="Comic Sans MS", size=60)
+
+        self.m2_label = Label(master, text="Mass 2:", font=self.title_font,fg="Black",bg="Gold")
+        self.m2_label.grid(row=3, column=1, sticky="e"+'w')
+
+        self.m2_entry = Entry(master, textvariable=self.m2, font=self.title_font, width=10, justify=CENTER,fg="White",bg="Black")
+        self.m2_entry.grid(row=3, column=2, sticky="w"+"e")
+
+        self.m1_label = Label(master, text="Mass 1:", font=self.title_font,fg="Black",bg="Gold")
+        self.m1_label.grid(row=2, column=1, sticky="e"+"w")
+
+        self.m1_entry = Entry(master, textvariable=self.m1, font=self.title_font, width=10, justify=CENTER,fg="White",bg="Black")
+        self.m1_entry.grid(row=2, column=2, sticky="w"+"e")
+
+        self.r_label = Label(master, text="Radius:", font=self.title_font,fg="Black",bg="Gold")
+        self.r_label.grid(row=4, column=1, sticky="e"+"w")
+
+        self.r_entry = Entry(master, textvariable=self.r, font=self.title_font, width=10, justify=CENTER,fg="White",bg="Black")
+        self.r_entry.grid(row=4, column=2, sticky="w"+"e")
+
+        self.grav_button = Label(master, text="Calculate : ",font=self.title_font,pady=2,borderwidth=5,relief="raised",height=1,fg="White",bg="Black")
+        self.grav_button.grid(row=5, column=1, sticky="e"+"w")
+
+        self.grav_button.bind("<Button-1>",self.concat)
+
+        self.grav_label = Label(master, textvariable=self.grav, font=self.title_font, width=10, borderwidth=5,relief="raised",fg="Black",bg="Gold",justify=CENTER)
+        self.grav_label.grid(row=5, column=2, sticky="w"+"e")
+
+    def concat(self,event):
+        try:
+            self.grav.set(self.m1.get() * self.m2.get() / (self.r.get() * self.r.get()) * 0.0000000000667)
+        except ZeroDivisionError:
+            self.grav.set("Not divisible by 0")
+        except:
+            self.grav.set("Type Error")
+
+
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Universal Gravity Calculator - UGC")
+    my_app = App(root)
+    root.mainloop()
 
